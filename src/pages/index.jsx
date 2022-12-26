38d57f29fe228d49ebbff59f1d33a65a09ecfc59
nv21059 <118710586+nv21059@users.noreply.github.com>
2022-12-26 10:16:54 +0300

    updated title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -264,8 +264,7 @@ export default function Home({ articles }) {
             Software designer, Cloud Artifact.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
+            I’m Ahmed, a software designer and Cloud Artifact based in Bahrain. I’m the founder and CEO of Planetaria, where we develop
             technologies that empower regular people to explore space on their
             own terms.
           </p>
